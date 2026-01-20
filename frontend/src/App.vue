9845bfb914325f1753b36ef97a7172a2885f667a
dorianrobineau35 <dorianrobineau35@gmail.com>
2026-01-20 17:41:47 +0100

    Fix majuscule App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<style>
+body {
+  font-family: 'Poppins', sans-serif;
+  background: #1b1336;
+  color: white;
+  margin: 0;
+}
+</style>
